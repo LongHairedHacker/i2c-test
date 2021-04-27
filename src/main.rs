d37b2e555d4b9b360b3221f4346f392de488ae4f
LongHairedHacker <sebastian@sebastians-site.de>
2021-04-27 21:32:07 +0200

    Minimal reproducer

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+#![no_std]
+#![no_main]
+
+//use wspr_beacon::application;
+
+use core::sync::atomic::{AtomicUsize, Ordering};
+use defmt_rtt as _; // global logger
+use panic_probe as _;
+use stm32f1xx_hal as _;
+use stm32f1xx_hal::{i2c, pac, prelude::*};
+
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
+}
+
+static COUNT: AtomicUsize = AtomicUsize::new(0);
+defmt::timestamp!("{=usize}", {
+    // NOTE(no-CAS) `timestamps` runs with interrupts disabled
+    let n = COUNT.load(Ordering::Relaxed);
+    COUNT.store(n + 1, Ordering::Relaxed);
+    n
+});
+
+/// Terminates the application and makes `probe-run` exit with exit-code = 0
+pub fn exit() -> ! {
+    loop {
+        cortex_m::asm::bkpt();
+    }
+}
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    // Get access to the core peripherals from the cortex-m crate
+    //let cp = cortex_m::Peripherals::take().unwrap();
+    // Get access to the device specific peripherals from the peripheral access crate
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
+    // HAL structs
+    let mut flash = dp.FLASH.constrain();
+
+    let mut rcc = dp.RCC.constrain();
+
+    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
+    // `clocks`
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.mhz())
+        .sysclk(72.mhz())
+        .pclk1(36.mhz())
+        .freeze(&mut flash.acr);
+
+    defmt::info!("Clock Setup done");
+
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    let scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
+    let sda = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
+    let mut i2c = i2c::BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        i2c::Mode::Standard {
+            frequency: 400_000.hz(),
+            //duty_cycle: i2c::DutyCycle::Ratio2to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    defmt::info!("I2C Setup done");
+
+    let res = i2c.write(96, &[3, 0]);
+    if res.is_err() {
+        panic!("i2c write failed.")
+    }
+
+    defmt::info!("Done");
+
+    loop {}
+}
